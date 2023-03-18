7980b9e33a3049423eb9886adb35b0d29af290bf
rjlewis1592 <rjlewis1592@gmail.com>
2023-03-18 23:26:42 +0000

    add CLI args for main funciton

--- hello_world.c
+++ hello_world.c
@@ -7,6 +7,11 @@ void print_default()
 
 int main(int argc, char *argv[])
 {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: <a.out> <animal_str>\n");
+		exit (EXIT_FAILURE);
+	}
+
 	print_default();
 	return 0;
 }
